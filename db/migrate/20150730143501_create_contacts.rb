5056a07c22f772664580c2a4f2600961762617b4
Josue Martinez <josuedaniel831@yahoo.com>
2015-07-30 14:56:07 +0000

    added contacts controller

--- db/migrate/20150730143501_create_contacts.rb
+++ db/migrate/20150730143501_create_contacts.rb
@@ -1,6 +1,13 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      t.string :Name
+      t.string :Email
+      t.text :Comments
+      
+      t.timestamps 
+      
+      
     end
   end
 end
