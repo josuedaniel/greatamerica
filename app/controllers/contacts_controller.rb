7b846f75df03d437efea6eef434c331ada4f0401
Josue Martinez <josuedaniel831@yahoo.com>
2015-08-04 15:51:48 +0000

    added form validations and edited application

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,23 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact= Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = 'Message Sent.'
+            redirect_to new_contact_path
+        else
+            flash[:error] = 'Error Occured, message has not been sent.'
+            redirect_to new_contact_path
+        end
+    
     end
-end
+    
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :DOB, :SS, :Current_Address, :city, :state, :zip, :how_long?, :dependents, :home_phone_number, :Cell_number, :rent_payment, :Landlord, :phone_number, :name_on_lease?, :employer, :occupation, :how_long_at_work?, :supervisor, :phone_number, :monthly_income)
+        end
+    end
+
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,23 @@
 class Contact < ActiveRecord::Base
-
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :DOB, presence: true
+    validates :SS, presence: true
+    validates :Current_Address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
+    validates :how_long?, presence: true
+    validates :dependents, presence: true
+    validates :home_phone_number, presence: true
+    validates :Cell_number, presence: true
+    validates :rent_payment, presence: true
+    validates :Landlord, presence: true
+    validates :phone_number, presence: true
+    validates :name_on_lease?, presence: true
+    validates :employer, presence: true
+    validates :occupation, presence: true
+    validates :supervisor, presence: true
+    validates :monthly_income, presence: true
+    
 end
