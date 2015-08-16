053cf3645812bed068434cbb47ad114d40febf86
Josue Martinez <josuedaniel831@yahoo.com>
2015-08-16 20:35:45 +0000

    fix validations bug

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,17 +7,6 @@ class Contact < ActiveRecord::Base
     validates :city, presence: true
     validates :state, presence: true
     validates :zip, presence: true
-    validates :how_long?, presence: true
-    validates :dependents, presence: true
-    validates :home_phone_number, presence: true
-    validates :Cell_number, presence: true
-    validates :rent_payment, presence: true
-    validates :Landlord, presence: true
-    validates :phone_number, presence: true
-    validates :name_on_lease?, presence: true
-    validates :employer, presence: true
-    validates :occupation, presence: true
-    validates :supervisor, presence: true
-    validates :monthly_income, presence: true
+    
     
 end
